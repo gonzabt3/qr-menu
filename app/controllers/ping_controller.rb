2bf919f1351b801cb5950ed90b42a82ee07d7c0c
gonza <gonzalo.muscolo@gmail.com>
2024-11-11 19:19:57 -0300

    fix env vars

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/ping_controller.rb
 class PingController < ApplicationController
   def index
-    render json: { message: 'pong' }
+    render json: { message: "pong #{ENV['CORS_ALLOWED_ORIGINS']}" }
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ALLOWED_ORIGIN']
+    origins ENV['CORS_ALLOWED_ORIGINS']
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
