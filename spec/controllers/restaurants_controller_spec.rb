a5044cfa820509f344ea6c88eb900e26575aa633
gonza <gonzalo.muscolo@gmail.com>
2024-12-29 19:59:13 -0300

    rubcop

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -1,10 +1,10 @@
- #spec/requests/restaurants_controller_spec.rb
+# spec/requests/restaurants_controller_spec.rb
 require 'rails_helper'
 
-RSpec.describe "Restaurants", type: :request do
+RSpec.describe 'Restaurants', type: :request do
   let(:user) { create(:user) }
   let(:restaurant) { create(:restaurant, user: user) }
-  let(:valid_attributes) {
+  let(:valid_attributes) do
     {
       name: 'Test Restaurant',
       address: '123 Test St',
@@ -14,103 +14,101 @@ RSpec.describe "Restaurants", type: :request do
       instagram: 'http://instagram.com/test',
       description: 'A test restaurant'
     }
-  }
-  let(:invalid_attributes) {
+  end
+  let(:invalid_attributes) do
     { name: nil }
-  }
+  end
   before do
     allow_any_instance_of(ApplicationController).to receive(:authorize).and_return(true)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 
-
-  describe "GET /restaurants" do
-    it "returns a success response" do
+  describe 'GET /restaurants' do
+    it 'returns a success response' do
       restaurant
       get restaurants_path
       expect(response).to be_successful
     end
   end
 
-  describe "GET /users/:email/restaurants" do
-    it "returns a success response" do
+  describe 'GET /users/:email/restaurants' do
+    it 'returns a success response' do
       get restaurants_user_path(user.email)
       expect(response).to be_successful
     end
 
-    it "returns a not found response if user does not exist" do
+    it 'returns a not found response if user does not exist' do
       get restaurants_user_path('nonexistent@example.com')
       expect(response).to have_http_status(:not_found)
     end
   end
 
-   describe "GET /restaurants/:id" do
+  describe 'GET /restaurants/:id' do
     let(:restaurant1) { create(:restaurant, user: user) }
-     it "returns a success response" do
-       byebug
-       get restaurant_path(restaurant1)
-       expect(response).to be_successful
-     end
-   end
+    it 'returns a success response' do
+      get restaurant_path(restaurant1)
+      expect(response).to be_successful
+    end
+  end
 
-   describe "POST /restaurants" do
-     context "with valid params" do
-       it "creates a new Restaurant" do
-         expect {
-           post restaurants_path, params: { restaurant: valid_attributes }
-         }.to change(Restaurant, :count).by(1)
-       end
+  describe 'POST /restaurants' do
+    context 'with valid params' do
+      it 'creates a new Restaurant' do
+        expect do
+          post restaurants_path, params: { restaurant: valid_attributes }
+        end.to change(Restaurant, :count).by(1)
+      end
 
-       it "renders a JSON response with the new restaurant" do
-         post restaurants_path, params: { restaurant: valid_attributes }
-         expect(response).to have_http_status(:created)
-         expect(response.content_type).to eq('application/json; charset=utf-8')
-       end
-     end
+      it 'renders a JSON response with the new restaurant' do
+        post restaurants_path, params: { restaurant: valid_attributes }
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
 
-     context "with invalid params" do
-       it "renders a JSON response with errors for the new restaurant" do
-         post restaurants_path, params: { restaurant: invalid_attributes }
-         expect(response).to have_http_status(:unprocessable_entity)
-         expect(response.content_type).to eq('application/json; charset=utf-8')
-       end
-     end
-   end
+    context 'with invalid params' do
+      it 'renders a JSON response with errors for the new restaurant' do
+        post restaurants_path, params: { restaurant: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+  end
 
-   describe "PUT /restaurants/:id" do
-     context "with valid params" do
-       let(:new_attributes) {
-         { name: 'Updated Restaurant' }
-       }
+  describe 'PUT /restaurants/:id' do
+    context 'with valid params' do
+      let(:new_attributes) do
+        { name: 'Updated Restaurant' }
+      end
 
-       it "updates the requested restaurant" do
-         put restaurant_path(restaurant), params: { restaurant: new_attributes }
-         restaurant.reload
-         expect(restaurant.name).to eq('Updated Restaurant')
-       end
+      it 'updates the requested restaurant' do
+        put restaurant_path(restaurant), params: { restaurant: new_attributes }
+        restaurant.reload
+        expect(restaurant.name).to eq('Updated Restaurant')
+      end
 
-       it "renders a JSON response with the restaurant" do
-         put restaurant_path(restaurant), params: { restaurant: valid_attributes }
-         expect(response).to have_http_status(:ok)
-         expect(response.content_type).to eq('application/json; charset=utf-8')
-       end
-     end
+      it 'renders a JSON response with the restaurant' do
+        put restaurant_path(restaurant), params: { restaurant: valid_attributes }
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
 
-     context "with invalid params" do
-       it "renders a JSON response with errors for the restaurant" do
-         put restaurant_path(restaurant), params: { restaurant: invalid_attributes }
-         expect(response).to have_http_status(:unprocessable_entity)
-         expect(response.content_type).to eq('application/json; charset=utf-8')
-       end
-     end
-   end
+    context 'with invalid params' do
+      it 'renders a JSON response with errors for the restaurant' do
+        put restaurant_path(restaurant), params: { restaurant: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+  end
 
-   describe "DELETE /restaurants/:id" do
-     it "destroys the requested restaurant" do
-       restaurant
-       expect {
-         delete restaurant_path(restaurant)
-       }.to change(Restaurant, :count).by(-1)
-     end
-   end
+  describe 'DELETE /restaurants/:id' do
+    it 'destroys the requested restaurant' do
+      restaurant
+      expect do
+        delete restaurant_path(restaurant)
+      end.to change(Restaurant, :count).by(-1)
+    end
+  end
 end
