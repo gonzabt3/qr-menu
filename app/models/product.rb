92a90810f0c3006ae356838684ccd6f16815eae0
= <gonzalo.muscolo@gmail.com>
2025-02-14 01:20:49 -0300

    delete from s3 at remove section

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,24 @@
 # app/models/product.rb
 class Product < ApplicationRecord
   belongs_to :section
-
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  before_destroy :delete_image_on_s3, if: -> { image_url.present? }
+  
+  
+  def delete_image_on_s3
+    client = Aws::S3::Client.new(
+      region: ENV['AWS_REGION'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
+    extension = image_url.split('.').last
+    s3_path = "menus/#{section.menu.id}/products/#{id}.#{extension}"
+
+    client.delete_object({
+                           bucket: ENV['S3_BUCKET_NAME'],
+                           key: s3_path
+                         })
+  end
+
 end
