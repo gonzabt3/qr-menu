3b24de5872be13b6be2c15c6da9da652d46267c8
gonza <gonzalo.muscolo@gmail.com>
2026-01-18 22:24:18 -0300

    Fix Puma configuration: correct port binding and ensure worker timeout is set for development

--- config/puma.rb
+++ config/puma.rb
@@ -22,11 +22,11 @@ if ENV['RAILS_ENV'] == 'production'
   require 'concurrent-ruby'
   worker_count = Integer(ENV.fetch('WEB_CONCURRENCY') { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
-endfetch('PORT') { 3000 }
-bind "tcp://0.0.0.0:#{port_number}"
+end
 
-# Specifies the `environment` that Puma will run in.
-environment ENV.fetch('RAILS_ENV') { 'development' }
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
