e3714fe48c79378945f176a79f374db9fedd7ab7
gonza <gonza@github.com>
2026-01-22 00:19:35 -0300

    Add logo_url column to restaurants table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_23_184207) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "logo_url"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
